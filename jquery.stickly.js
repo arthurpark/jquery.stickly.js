68d38af782a20d2639068086334aa34633cd9024
Arthur Park <arthurh.park@gmail.com>
2013-02-04 12:50:57 -0800

    Make use of CSS position: fixed
    Instead of changing top: on scroll, use position: fixed when the scroll
    hits top and bottom boundaries (of container) to prevent shaky behavior.

--- jquery.stickly.js
+++ jquery.stickly.js
@@ -1,31 +1,89 @@
-(function(window, $) {
+/**
+ * jquery.stickly.js
+ * stickly is a jQuery plugin that makes an element inside a container
+ * sticky (fixed) as browser is scrolled vertically.
+ *
+ * @name stickly
+ * @author Tom Jakubowski, Arthur Park
+ *
+ * Usage:
+ *     $('.stickly').stickly();
+ *     $('.stickly').stickly({ top: 50 });
+ *
+ * Source and example available at:
+ * https://github.com/tomjakubowski/jquery.stickly.js
+ *
+ */
+(function(window,$) {
+    "use strict";
 
-  'use strict';
+    var defaults = {
+        // This is used as top when stickly's position: fixed
+        top: 0,
+        // Default container is direct parent.
+        container: null
+    };
 
-  $.fn.stickly = function(container) {
-    var $container = $(container),
-        $sticky = $(this);
+    $.fn.stickly = function(options) {
+        // Cache values that doesn't change after intialization to lighten up process function which is
+        // called on every scroll event.
+        var config = $.extend({}, defaults, options),
+            $container = (config.container) ? $(config.container) : this.parent(),
+            $me = this,
+            sticklyHeight = $me.height(),
+            containerHeight = $container.outerHeight(),
+            containerBottom = $container.offset().top + containerHeight,
+            containerPaddingTop = $container.css('padding-top') ? parseInt($container.css('padding-top'), 10) : 0,
+            containerPaddingBottom = $container.css('padding-bottom') ? parseInt($container.css('padding-bottom'), 10) : 0,
+            topBound    = $me.offset().top - config.top,
+            bottomBound = containerBottom - sticklyHeight - config.top - containerPaddingBottom,
+            isSticky    = false;
 
-    $(window).on('scroll.stickly', process());
+        $(window).on("scroll.stickly", process);
 
-    function process() {
-      var minBound = $container.offset().top,
-          maxBound = minBound + $container.height();
+        /**
+         * This function goes through some conditionals, and change style position: only when the $me
+         * hits boundary.
+         */
+        function process() {
+            var scrollTop = $(document).scrollTop(),
+                containerPositionLeft = $container.offset().left,
+                containerPaddingLeft = $container.css('padding-left') ? parseInt($container.css('padding-left'), 10) : 0;
 
-      var minPosTop = $sticky.position().top,
-          maxPosTop = $container.height() - $sticky.height() - minPosTop;
-
-      return function(e) {
-          var scrollTop = $(window).scrollTop(),
-              newPosTop = scrollTop - minBound + minPosTop;
-
-          newPosTop = Math.max(newPosTop, minPosTop);
-          newPosTop = Math.min(newPosTop, maxPosTop);
-
-          $sticky.css('top', newPosTop);
-      };
-    }
-  };
-
+            if ( !isSticky ) {
+                // While scrolling down, when scroll position hits stickly's top, make stickly fixed.
+                if (scrollTop > topBound && scrollTop < bottomBound) {
+                    $me.css({
+                        'position': 'fixed',
+                        'top':  ((config.top) ? config.top + 'px' : 0),
+                        'bottom': '',
+                        'left': (containerPositionLeft + containerPaddingLeft) + 'px'
+                    });
+                    isSticky = true;
+                }
+            } else {
+                if ($me.css('position') === 'fixed') {
+                    if (scrollTop >= bottomBound) {
+                        // When stickly's bottom hit's bottom thrashhold
+                        $me.css({
+                            'position': 'absolute',
+                            'top': '',
+                            'bottom' : containerPaddingBottom + 'px',
+                            'left': (containerPaddingLeft ? containerPaddingLeft + 'px' : 0)
+                        });
+                        isSticky = false;
+                    } else if (scrollTop <= topBound || scrollTop <= 0) {
+                        // When stickly's top hits its original top position.
+                        $me.css({
+                            'position': 'absolute',
+                            'top': containerPaddingTop + 'px',
+                            'bottom': '',
+                            'left': (containerPaddingLeft ? containerPaddingLeft + 'px' : 0)
+                        });
+                        isSticky = false;
+                    }
+                }
+            }
+        }
+    };
 })(this, this.jQuery);
-
